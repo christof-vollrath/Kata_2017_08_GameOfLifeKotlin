82c601604c58a4c4f1f465082deca599d964e7db
Christof Vollrath <christof@taobits.net>
2017-09-04 17:08:12 +0200

    More Kotlinification

--- src/test/kotlin/GameOfLife.kt
+++ src/test/kotlin/GameOfLife.kt
@@ -92,45 +92,32 @@ data class Cell(val x: Int, val y: Int)
 
 data class Board(val cells: HashSet<Cell> = HashSet<Cell>()) {
     fun isAlive(cell: Cell) = cells.contains(cell)
-    fun setAlive(cell: Cell): Board {
-        cells.add(cell)
-        return this
-    }
+    fun setAlive(cell: Cell) = apply { cells.add(cell) }
     fun isEmpty() = cells.isEmpty()
 
-    fun calculateNextGeneration(): Board {
+    fun calculateNextGeneration() = let {
         val nextGeneration = cells.filter { shouldBeAliveInNextGeneration(it) } +
-                             reproductionCandidates().filter { shouldBeRebornInNextGeneration(it) }
-        return Board(nextGeneration.toHashSet())
+                reproductionCandidates().filter { shouldBeRebornInNextGeneration(it) }
+        Board(nextGeneration.toHashSet())
     }
 
-    fun reproductionCandidates(): Set<Cell> =
-        cells.flatMap { getAllEmptyNeighbors(it) }.toHashSet()
+    fun reproductionCandidates(): Set<Cell> = cells.flatMap { getAllEmptyNeighbors(it) }.toHashSet()
 
-    fun shouldBeAliveInNextGeneration(cell: Cell) =
-            shouldBeAliveInNextGeneration(countLivingNeighbors(cell))
-    fun shouldBeAliveInNextGeneration(nrNeighbors: Int) =
-        nrNeighbors == 2 || nrNeighbors == 3
+    fun shouldBeAliveInNextGeneration(cell: Cell) = shouldBeAliveInNextGeneration(countLivingNeighbors(cell))
+    fun shouldBeAliveInNextGeneration(nrNeighbors: Int) = nrNeighbors == 2 || nrNeighbors == 3
 
-    fun shouldBeRebornInNextGeneration(cell: Cell) =
-            shouldBeRebornInNextGeneration(countLivingNeighbors(cell))
-    fun shouldBeRebornInNextGeneration(nrNeighbors: Int) =
-            nrNeighbors == 3
+    fun shouldBeRebornInNextGeneration(cell: Cell) = shouldBeRebornInNextGeneration(countLivingNeighbors(cell))
+    fun shouldBeRebornInNextGeneration(nrNeighbors: Int) = nrNeighbors == 3
 
-    fun countLivingNeighbors(cell: Cell): Int =
-        getAllNeighbors(cell).filter { isAlive(it) }.count()
+    fun countLivingNeighbors(cell: Cell): Int = getAllNeighbors(cell).filter { isAlive(it) }.count()
 
-    fun getAllEmptyNeighbors(cell: Cell): Set<Cell> {
-        return getAllNeighbors(cell).minus(cells)
-    }
+    fun getAllEmptyNeighbors(cell: Cell): Set<Cell> = getAllNeighbors(cell).minus(cells)
 
-    fun getAllNeighbors(cell: Cell): Set<Cell> {
-        val result = HashSet<Cell>()
+    fun getAllNeighbors(cell: Cell): Set<Cell> = HashSet<Cell>().apply {
         for (dx in -1..1)
             for (dy in -1..1)
                 if (dx != 0 || dy != 0)
-                    result.add(Cell(cell.x + dx, cell.y + dy))
-        return result
+                    add(Cell(cell.x + dx, cell.y + dy))
     }
 }
 
